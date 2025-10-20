f687d37a243dc89d7d7a2c0cfd4491476fd1ac11
Prevail <mukungaprevail@gmail.com>
2025-10-20 15:51:05 +0200

    Deleted files

---
+++ Local Language  Phrasebook/Language Category page.swift
@@ -0,0 +1,14 @@
+//
+//  Ndebele Category page.swift
+//  Local Language  Phrasebook
+//
+//  Created by Prevail Mukunga on 20/10/2025.
+//
+
+import SwiftUI
+
+struct LanguageCategoryPage: View {
+    var body: some View {
+        Text("Language Category Page")
+    }
+}
---
+++ Local Language  Phrasebook/Language Selection Page.swift
@@ -0,0 +1,61 @@
+//
+//  Language Category Page .swift
+//  Local Language  Phrasebook
+//
+//  Created by Prevail Mukunga on 20/10/2025.
+//
+
+import SwiftUI
+
+
+struct LanguageSelectionPage: View {
+    var body: some View {
+        NavigationStack{
+            ZStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(AngularGradient(gradient: Gradient(colors: [Color.green, Color.white]), center: .bottom, angle: .degrees(355)))
+                    .frame(width: 430, height: 990)
+                VStack {
+                    
+                    Text("Select Language")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(30)
+                  
+                    NavigationLink{LanguageCategoryPage()}label: {
+                        ZStack{
+                          RoundedRectangle(cornerRadius: 20)
+                                .fill(AngularGradient(gradient: Gradient(colors: [Color.white, Color.green]), center: .bottom, angle: .degrees(365)))
+                                .frame(width: 390, height: 90)
+                            Text("Ndebele")
+                                .foregroundColor(.indigo)
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .fontDesign(.rounded)
+                        }
+                    }
+                            
+                    NavigationLink{LanguageCategoryPage()}label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(AngularGradient(gradient: Gradient(colors: [Color.white, Color.green]), center: .bottom, angle: .degrees(365)))
+                                .frame(width: 390, height: 90)
+                       Text("Shona")
+                                .foregroundColor(.indigo)
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .fontDesign(.rounded)
+                        }
+                    }
+                            
+                    
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    LanguageSelectionPage()
+}
