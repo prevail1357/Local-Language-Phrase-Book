65c41e19b1e44e016f0736a280758cca2decd588
Prevail <mukungaprevail@gmail.com>
2025-10-27 16:52:12 +0200

    added a side menu bar and updated the search bar to be visible on simulator

---
+++ AudioTracks/Local Language  Phrasebook/SideMenuRowView.swift
@@ -0,0 +1,40 @@
+//
+//  SideMenuRowView.swift
+//  Local Language  Phrasebook
+//
+//  Created by Prevail Mukunga on 27/10/2025.
+//
+
+import SwiftUI
+
+struct SideMenuRowView: View {
+    
+    let option: SideMenuOptionModel
+    @Binding var selectedOption: SideMenuOptionModel?
+    
+    private var isSelected: Bool {
+        return selectedOption == option
+    }
+    
+    var body: some View {
+        HStack{
+            Image(systemName: option.systemImageName)
+                .imageScale(.large)
+            
+            Text(option.title)
+                .font(.headline)
+            
+            Spacer()
+               
+        }
+        .padding(.leading)
+        .foregroundStyle(isSelected ? .green : .primary)
+        .frame(width: 216, height: 44)
+        .background(isSelected ? .green.opacity(0.20) : .clear )
+       
+    }
+}
+
+#Preview {
+    SideMenuRowView(option: .faivorites, selectedOption: .constant(.faivorites))
+}
